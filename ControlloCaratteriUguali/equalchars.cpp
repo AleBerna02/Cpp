3d69b1a311c55e411ea6ca9bc2791293bca92315
AleBerna02 <alessiobernardello@hotmail.com>
2026-02-26 00:39:25 +0100

    Esercitazioni C++

---
+++ ControlloCaratteriUguali/equalchars.cpp
@@ -0,0 +1,16 @@
+// File creato da Bernardello Alessio
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a, b;
+    cin >> a >> b;
+
+    if (a == b) {
+        cout << "Uguali" << endl;
+    } else {
+        cout << "Diversi" << endl;
+    }
+
+    return 0;
+}
